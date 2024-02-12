ec64a0369dea8bf0593de11c67c37d9733c2adff
RoelYoon <roel.yoon@ggq.gg>
2024-02-12 17:38:10 +0900

    Solved Acowdemia I

---
+++ Bronze/2021 Open/Acowdemia I/acowdemia1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n,L; 
+    cin>>n>>L;
+    vector<int> c(n);
+    for(int i = 0; i < n; i++){
+        cin>>c[i];
+    }
+    sort(c.begin(),c.end());
+    vector<bool> cited(n,false);
+    int h = 1;
+    for(int i = n-1; i >= 0; i--){
+        if(c[i]<h){
+            if(L>0){
+                int j=i;
+                while(j<n && c[j]< h && L>0){
+                    if(cited[j]){goto end;}
+                    c[j]++;
+                    cited[j]=true;
+                    L--;
+                    j++;
+                }if(j>n){j--;}
+                if(c[i]<h || c[j]<h || c[n-1]<h){
+                    break;
+                }
+            }else{
+                break;
+            }
+        }
+        h++;
+    }
+    end: 
+    cout<<h-1<<"\n";
+}
