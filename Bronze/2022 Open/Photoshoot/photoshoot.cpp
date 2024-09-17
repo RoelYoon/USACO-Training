8d4f6d70293562e720cc9f2993b29cfd71cd8c24
Roel Yoon <roelyoon2@gmail.com>
2024-09-17 17:06:38 +0900

    Solved Photoshoot

--- Bronze/2022 Open/Photoshoot/photoshoot.cpp
+++ Bronze/2022 Open/Photoshoot/photoshoot.cpp
@@ -1,29 +1,28 @@
-//Unfinished
 #include <iostream>
+#include <bitset>
+#define MAX 100005
 using namespace std;
-void reverse(string& s, int k){
-    for(int i = 0; i <= k/2; i++){
-        char temp = s[i];
-        s[i] = s[k-i]; 
-        s[k-i] = temp; 
-    }
-}
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
     int n; cin>>n;
     string s; cin>>s;
-    int count = 0; 
-    for(int i = n-1; i >= 0; i-=2){
-        int score = 0; 
-        for(int j = 1; j <= i; j+=2){
-            if(s[j]=='G' && s[i-j]=='H'){score--;}
-            else if(s[j]=='H' && s[i-j]=='G'){score++;}
-        }
-        if(score>0){
-            reverse(s,i);
-            count++;
+    bitset<MAX> ps(0);
+    int j = 0;
+    for(int i = 0; i < n; i+=2){
+        string ss = s.substr(i,2);
+        if(ss == "GH"){
+            j++;
+        }else if(ss=="HG"){
+            ps[j++]=1;
         }
     }
-    cout<<count<<"\n";
+    int cnt = 0;
+    for(int i = j-1; i >= 0; --i){
+        if(!ps[i]){
+            for(int k = 0; k <=i; ++k){
+                ps[k].flip();
+            }
+            ++cnt;
+        }
+    }
+   cout<<cnt<<"\n";
 }
