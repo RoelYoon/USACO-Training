a55fcaf556ae08007b84ede13d413bacd69b3315
Roel Yoon <roelyoon2@gmail.com>
2024-09-17 15:05:25 +0900

    Solved Counting Liars

--- Bronze/2022 Open/Counting Liars/liars.cpp
+++ Bronze/2022 Open/Counting Liars/liars.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int n;
-bool greaterMap[1005];
-long long placeMap[1005];
-bool agrees(int truthID,int cow2ID){
-    return greaterMap[truthID]==greaterMap[cow2ID] || placeMap[truthID]==placeMap[cow2ID] ||
-    (placeMap[truthID]>placeMap[cow2ID] && greaterMap[cow2ID] && !greaterMap[truthID]) ||
-    (placeMap[truthID]<placeMap[cow2ID] && !greaterMap[cow2ID] && greaterMap[truthID]);
-}
+struct Info{
+    bool GE;
+    int p;
+    Info(){}
+    Info(bool g , int p):GE(g),p(p){}
+};
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cin>>n;
-    int maxTruthValue=0;
-    for(int i = 0; i < n; i++){
-        char temp;
-        cin>>temp>>placeMap[i];
-        greaterMap[i]=temp=='G'?1:0;
-    }
-    for(int cow1 = 0; cow1 < n; cow1++){
-        int truthValue = 0;
-        for(int cow2 = 0; cow2 < n; cow2++){
-            if(agrees(cow1,cow2)){
-                truthValue++;
-            }
-        }
-        maxTruthValue = maxTruthValue>truthValue?maxTruthValue:truthValue;
-    }
-    cout<<n-maxTruthValue<<"\n";
+   int n; cin>>n;
+   vector<Info> infos(n);
+   for(int i = 0; i < n; ++i){
+        char g; cin>>g>>infos[i].p;
+        infos[i].GE=g=='G';
+   }
+   int minLiar = 1000000000;
+   for(int i = 0; i < n; ++i){
+       int cnt = 0; 
+       for(int j = 0; j < n; ++j){
+           if(infos[j].GE){
+               cnt+=infos[i].p<infos[j].p;
+           }else{
+               cnt+=infos[i].p>infos[j].p;
+           }
+       }
+       minLiar = min(minLiar,cnt);
+   }
+   cout<<minLiar<<"\n";
 }
